9b5f759d65caf40f988598adb2d3c812aad6d1b5
andreas14101 <andreasshansen@live.dk>
2023-03-16 12:30:15 +0100

    db

---
+++ src/DAL/DatabaseConnector.java
@@ -0,0 +1,39 @@
+package DAL;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.Properties;
+
+public class DatabaseConnector {
+    private static final String PROP_FILE = "config/database.settings";
+    private SQLServerDataSource ds;
+
+    public DatabaseConnector() throws IOException {
+
+
+        Properties databaseProperties = new Properties();
+        databaseProperties.load(new FileInputStream(new File(PROP_FILE)));
+
+        String server = databaseProperties.getProperty("Server");
+        String database = databaseProperties.getProperty("Database");
+        String user = databaseProperties.getProperty("User");
+        String password = databaseProperties.getProperty("Password");
+
+        ds = new SQLServerDataSource();
+        ds.setServerName(server);
+        ds.setDatabaseName(database);
+        ds.setUser(user);
+        ds.setPassword(password);
+        ds.setTrustServerCertificate(true);
+    }
+
+    public Connection getConnection() throws SQLServerException {
+        return ds.getConnection();
+    }
+
+}
