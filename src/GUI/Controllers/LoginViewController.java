d76fdccb3c885cc4dd89b2a490cee22bfa651fee
andreas14101 <andreasshansen@live.dk>
2023-03-17 11:52:08 +0100

    login css

--- src/GUI/Controllers/LoginViewController.java
+++ src/GUI/Controllers/LoginViewController.java
@@ -1,4 +1,21 @@
 package GUI.Controllers;
 
-public class LoginViewController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class LoginViewController extends BaseController {
+
+    @FXML
+    private Button BtnClose;
+    @Override
+    public void setup() {
+    }
+
+
+    public void handleClose(ActionEvent event) {
+        Stage stage = (Stage) BtnClose.getScene().getWindow();
+        stage.close();
+    }
 }
