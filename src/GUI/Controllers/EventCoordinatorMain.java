5af8c9f425ef99217e3b22db3a1926bacb8d022b
oliverager <114471849+oliverager@users.noreply.github.com>
2023-03-23 10:29:41 +0100

    EventCoordinatorView.fxml og Css

--- src/GUI/Controllers/EventCoordinatorMain.java
+++ src/GUI/Controllers/EventCoordinatorMain.java
@@ -1,4 +1,34 @@
 package GUI.Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
 public class EventCoordinatorMain {
+    public TextField txtEventName;
+    public TextField txtDate;
+    public TextField txtLocation;
+    public TextArea txaDescription;
+    public TextField txtTicketAmount;
+    public Button btnImageChooseFile;
+    public ImageView imgEvent;
+    public Label lblEventName;
+
+    public void handleCreateEvents(ActionEvent event) {
+    }
+
+    public void handleEditEvents(ActionEvent event) {
+    }
+
+    public void handleOpen(ActionEvent event) {
+    }
+
+    public void handleImageChooseFile(ActionEvent event) {
+    }
+
+    public void handleLogOut(ActionEvent event) {
+    }
 }
