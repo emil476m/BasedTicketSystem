c00545806f4f2270ab329dba1c5aa3b5adb52dfd
andreas14101 <andreasshansen@live.dk>
2023-04-11 14:26:06 +0200

    BCrypt

--- src/DAL/DB/LoginDAO_DB.java
+++ src/DAL/DB/LoginDAO_DB.java
@@ -5,6 +5,8 @@ import BE.Event_Coordinator;
 import BE.User;
 import DAL.DatabaseConnector;
 import DAL.Interfaces.ILogInDAO;
+import DAL.Util.BCrypt;
+import GUI.Controllers.BaseController;
 import com.microsoft.sqlserver.jdbc.SQLServerException;
 
 import java.io.IOException;
@@ -21,12 +23,11 @@ private DatabaseConnector dbConnector;
 
     @Override
     public User loginUser(String userName, String passWord) throws Exception {
-        String sql = "SELECT * FROM [User] Where UserName=? AND PassWord=?";
+        String sql = "SELECT * FROM [User] Where UserName=?";
         User user = null;
         try (Connection connection = dbConnector.getConnection();
              PreparedStatement statement = connection.prepareStatement(sql)){
                  statement.setString(1, userName);
-                 statement.setString(2,passWord);
 
                  ResultSet resultSet = statement.executeQuery();
 
@@ -39,12 +40,16 @@ private DatabaseConnector dbConnector;
                 int userTypes = resultSet.getInt("UserType");
                 user = new User(id,PassWord,UserName,mail,name,userTypes);
         }
+            if(BCrypt.checkpw(passWord,user.getPassWord()))
+            {
             return user;
+            }
         }
         catch (SQLException e)
         {
             e.printStackTrace();
             throw new Exception("Failed to login");
         }
+        return user;
     }
 }
