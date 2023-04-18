fa7d39a08983f86443a11f6538c60090e95a73c2
andreas14101 <andreasshansen@live.dk>
2023-04-18 10:45:09 +0200

    qr update

--- src/GUI/Util/QrCodeGenerator.java
+++ src/GUI/Util/QrCodeGenerator.java
@@ -9,7 +9,6 @@ import java.util.Map;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.NotFoundException;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
@@ -30,7 +29,6 @@ public class QrCodeGenerator {
     /**
      * creates a qrcode in a image file
      * @param data
-     * @param path
      * @param charSet
      * @param map
      * @param h
